e7b6d9bec17e2419c016ef52bccfeec1d12b3753
hassanroki <mdrokibulhassan786@gmail.com>
2026-05-12 16:38:36 +0600

    SMTP

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'homePage']);
@@ -15,3 +16,13 @@ Route::post('/tasks/store', [TaskController::class, 'store'])->name('task.store'
 Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('task.edit');
 Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('task.update');
 Route::delete('/tasks/{task}/delete', [TaskController::class, 'destroy'])->name('task.destroy');
+
+// Mail pit
+Route::get('/test-mail', function () {
+    Mail::raw('Testing Mail pit', function ($message) {
+        $message->to('john@example.com')
+            ->subject('Mail pit Test');
+    });
+
+    return 'Mail sent!';
+});
